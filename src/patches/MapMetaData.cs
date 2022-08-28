e36d9600a5ae161d417efba2d928430a1df1d676
CMiSSioN <47270663+CMiSSioN@users.noreply.github.com>
2022-08-28 22:48:15 +0300

    remove unnecessary load request

--- src/patches/MapMetaData.cs
+++ src/patches/MapMetaData.cs
@@ -11,18 +11,18 @@ namespace EnvironmentalDesignMasks {
         private static void Postfix(MapMetaData __instance) {
             try {
                 string mood = UnityGameInstance.BattleTechGame.Combat.ActiveContract.mapMood;
-                if (EDM.customMoods.ContainsKey(mood) && EDM.customMoods[mood].designMask != null) {
+                if (EDM.customMoods.ContainsKey(mood) && string.IsNullOrEmpty(EDM.customMoods[mood].designMask) == false) {
                     string designMask = EDM.customMoods[mood].designMask;
                     Traverse biomeDesignMaskName = Traverse.Create(__instance).Field("biomeDesignMaskName");
 
                     EDM.modLog.Info?.Write($"Overriding biomeDesignMaskName from {biomeDesignMaskName.GetValue()} to {designMask} based on mood {mood}");
-                    EDM.modLog.Info?.Write($"Adding loadRequest for DesignMaskDef: {designMask}");
+                    //EDM.modLog.Info?.Write($"Adding loadRequest for DesignMaskDef: {designMask}");
 
                     biomeDesignMaskName.SetValue(designMask);
 
-                    LoadRequest loadRequest = UnityGameInstance.BattleTechGame.DataManager.CreateLoadRequest();
-                    loadRequest.AddBlindLoadRequest(BattleTechResourceType.DesignMaskDef, designMask);
-                    loadRequest.ProcessRequests(1000U);
+                    //LoadRequest loadRequest = UnityGameInstance.BattleTechGame.DataManager.CreateLoadRequest();
+                    //loadRequest.AddBlindLoadRequest(BattleTechResourceType.DesignMaskDef, designMask);
+                    //loadRequest.ProcessRequests(1000U);
                 }
             } catch (Exception e) {
                 EDM.modLog.Error?.Write(e);
